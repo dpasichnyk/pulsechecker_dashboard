8b25177eb76ff86fd40a7c6bbdcd5b64157485f4
Sergei Koretskiy <sergei.koretskiy@gmail.com>
2021-01-31 17:16:39 +0200

    minor change seed file add dashboard_controller.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -34,5 +34,7 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "some#{n + 2}@example.com" }
     password { '12345678Qq' }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name  }
   end
 end
